81303c41be2209449c3857e40727a759534fd3a7
Tarimari <tarashorban1987@gmail.com>
2023-10-20 01:30:09 +0300

    throt

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,29 @@
 import Player from '@vimeo/player';
 
-console.dir(Player);
+console.dir();
 const iframe = document.querySelector('iframe');
 
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', _.throttle(onPlay, 1000));
 
 
-const onPlay = (evt) => {
-   console.dir(evt.currentTarget)
+function onPlay(evt) {
+    console.log(evt.seconds);
+   localStorage.setItem("videoplayer-current-time", `${evt.seconds}`);
 };
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
