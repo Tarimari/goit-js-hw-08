c5201a8e5d7e2c87df382a4d76e9cab699f28227
Tarimari <tarashorban1987@gmail.com>
2023-10-19 18:45:50 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
 
+console.dir(Player);
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+
+player.on('timeupdate', onPlay);
+
+
+const onPlay = (evt) => {
+   console.dir(evt.currentTarget)
+};
