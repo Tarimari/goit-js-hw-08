a86f5050e01059d08ae756dd1616c187056112ca
Tarimari <tarashorban1987@gmail.com>
2023-10-21 02:04:43 +0300

    work3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(`.feedback-form`);
+
+console.dir(form)
+
+form.addEventListener(`input`, throttle(handlerInput, 500, {
+      leading: true,
+      trailing: false,
+    }));
+
+function handlerInput(evt) {
+
+
+    let text = {
+        email: evt.currentTarget.elements.email.value,
+        message: evt.currentTarget.elements.message.value,
+    }
+    localStorage.setItem(`feedback-form-state`, JSON.stringify(text))
+    console.dir(evt.currentTarget.elements.email.value);
+    console.dir(evt.currentTarget.elements.message.value);
+
+}
