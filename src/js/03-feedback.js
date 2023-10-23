d240b8c38015adf9b215d1067c2a713255daef88
Tarimari <tarashorban1987@gmail.com>
2023-10-23 23:45:24 +0300

    all ok

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,9 +3,18 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector(`.feedback-form`);
 
 const dateInLS = localStorage.getItem(`feedback-form-state`);
+if (dateInLS)
+{
+if (JSON.parse(dateInLS).email)
+{ form.elements.email.value = JSON.parse(dateInLS).email };
+if (JSON.parse(dateInLS).message)
+    { form.elements.message.value = JSON.parse(dateInLS).message };
+}
+else
+{
+    localStorage.setItem(`feedback-form-state`, ``)
+}
 
-form.elements.email.value = JSON.parse(dateInLS).email || ``;
-form.elements.message.value = JSON.parse(dateInLS).message || ``;
 
 form.addEventListener(`submit`, handlerSubmit);
 
@@ -22,10 +31,12 @@ function handlerInput(evt) {
     localStorage.setItem(`feedback-form-state`, JSON.stringify(text))
     console.dir(evt.currentTarget.elements.email.value);
     console.dir(evt.currentTarget.elements.message.value);
-};
+}
 function handlerSubmit(evt) {
     evt.preventDefault();
     console.log(dateInLS);
     localStorage.clear();
+    form.elements.email.value = ``;
+    form.elements.message.value = ``;
 }
 
