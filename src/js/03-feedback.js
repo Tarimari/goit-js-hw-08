810fd273ef03caea56dd9cb2ae2ffb2b5878420f
Tarimari <tarashorban1987@gmail.com>
2023-10-24 15:10:33 +0300

    777

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,18 +2,23 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector(`.feedback-form`);
 
-const dateInLS = localStorage.getItem(`feedback-form-state`);
+const dateInLS = JSON.parse(localStorage.getItem(`feedback-form-state`));
+// const dateInLS = localStorage.getItem(`feedback-form-state`);
 if (dateInLS)
 {
-if (JSON.parse(dateInLS).email)
-{ form.elements.email.value = JSON.parse(dateInLS).email };
-if (JSON.parse(dateInLS).message)
-    { form.elements.message.value = JSON.parse(dateInLS).message };
-}
-else
-{
-    localStorage.setItem(`feedback-form-state`, ``)
-}
+    form.elements.email.value = dateInLS.email;
+    form.elements.message.value = dateInLS.message;
+    }
+// {
+// if (JSON.parse(dateInLS).email)
+// { form.elements.email.value = JSON.parse(dateInLS).email };
+// if (JSON.parse(dateInLS).message)
+//     { form.elements.message.value = JSON.parse(dateInLS).message };
+// }
+// else
+// {
+//     localStorage.setItem(`feedback-form-state`, ``)
+// }
 
 
 form.addEventListener(`submit`, handlerSubmit);
@@ -21,13 +26,11 @@ form.addEventListener(`submit`, handlerSubmit);
 form.addEventListener(`input`, throttle(handlerInput, 500));
 
 function handlerInput(evt) {
-    let text = {
-        email: evt.currentTarget.elements.email.value,
-        message: evt.currentTarget.elements.message.value,
+    const text = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
     }
     localStorage.setItem(`feedback-form-state`, JSON.stringify(text))
-    console.dir(evt.currentTarget.elements.email.value);
-    console.dir(evt.currentTarget.elements.message.value);
 }
 function handlerSubmit(evt) {
     evt.preventDefault();
